56002d0565d597a22e714957688b92e985258256
7erhiy <7erhiy@gmail.com>
2024-01-02 00:30:39 -0400

    add footer+header

---
+++ src/app/_footer/page.tsx
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <>
+      <div
+        className="footer"
+        style={{
+          backgroundColor: "black",
+          padding: "10px",
+          color: "#fff"
+        }}
+      >
+        <h1>Footer</h1>
+      </div>
+    </>
+  );
+}
---
+++ src/app/_header/page.tsx
@@ -0,0 +1,7 @@
+export default function Header () {
+  return (
+    <>
+    <h1>HEADER2</h1>
+    </>
+  )
+}
